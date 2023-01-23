3c9750581d43d9c75011c5d83418d7aefa8ec753
Dotwood Media <info@dotwood.media>
2023-01-23 20:45:55 +0100

    Deleted old code

--- events/interactionCreate.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-
-module.exports = async (client, interaction) => {
-    interaction = client.interactionFunctions(interaction, client);
-
-    if (interaction.isSelectMenu()) client.emit('selectionClick', interaction);
-    if (interaction.isButton()) client.emit('buttonClick', interaction);
-    if (interaction.isContextMenu()) client.emit('ContextClick', interaction);
-}
