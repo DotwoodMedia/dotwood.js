4b8eee3d6caf568cce0efee621c99fbdbd66f0a3
Dotwood Media <info@dotwood.media>
2023-01-24 20:23:13 +0100

    Created client config

---
+++ src/config/client.ts
@@ -0,0 +1,43 @@
+import { PresenceStatus } from "discord.js";
+
+export type IClientConfig = {
+    id: string;
+    guildId: string;
+    public: boolean;
+    colors: {
+        brand: string;
+        success: string;
+        error: string;
+        warning: string;
+    };
+    status: {
+        content: string[];
+        type: PresenceStatus;
+    };
+    dirs: {
+        commands: string;
+        events: string;
+    }
+}
+
+export default (config: IClientConfig): IClientConfig => {
+    return {
+        id: config.id,
+        guildId: config.guildId,
+        public: config.public || false,
+        colors: {
+            brand: config.colors.brand || '#5865F2',
+            success: config.colors.success || '#57F287',
+            error: config.colors.error || '#ED4245',
+            warning: config.colors.warning || '#FF9A00'
+        },
+        status: {
+            content: config.status.content || ["Dotwood.js", "https://www.npmjs.com/package/dotwood.js"],
+            type: config.status.type || 'online'
+        },
+        dirs: {
+            commands: config.dirs.commands || '/commands',
+            events: config.dirs.events || '/events'
+        }
+    }
+}
